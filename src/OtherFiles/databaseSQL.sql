f51a7036b358c254d97608c21c4202d40d6a54f6
travisnjenga <Travis.Mutungi@strathmore.edu>
2025-06-16 21:07:12 +0300

    Add or update databaseSQL.sql

--- src/OtherFiles/databaseSQL.sql
+++ src/OtherFiles/databaseSQL.sql
@@ -1,3 +1,7 @@
+-- Drink Enterprise Database Schema DDL for MySQL
+ CREATE DATABASE IF NOT EXISTS drink_enterprise_db;
+ USE drink_enterprise_db;
+
 CREATE TABLE IF NOT EXISTS branches (
     branch_id VARCHAR(50) PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
