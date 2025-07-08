6406888176411e8c2f096e3bd4f54eb1c6a63a66
Karanei Kimutai <168116985+Karanei-Kimutai@users.noreply.github.com>
2025-07-08 22:49:44 +0300

    Revert "Credential management"

--- src/Model/UtilitiesandServerEntryPoint/DatabaseManager.java
+++ src/Model/UtilitiesandServerEntryPoint/DatabaseManager.java
@@ -1,31 +1,18 @@
 package Model.UtilitiesandServerEntryPoint;
 
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.sql.*;
-import java.util.Properties;
 
 public class DatabaseManager {
-    private static final String DB_URL;
-    private static final String DB_USER;
-    private static final String DB_PASSWORD;
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/drink_enterprise_db?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+    private static final String DB_USER="root";
+    private static final String DB_PASSWORD="200517";
 
     static{
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
-            //Load database properties from an external file
-            Properties properties=new Properties();
-            FileInputStream inputStream=new FileInputStream("credentialsConfiguration.properties");
-            properties.load(inputStream);
-            DB_URL=properties.getProperty("databaseURL");
-            DB_USER=properties.getProperty("databaseUsername");
-            DB_PASSWORD=properties.getProperty("databasePassword");
         }catch(ClassNotFoundException e){
             System.err.println("FATAL: MySQL JDBC Driver not found. Please add mysql-connector-java.jar to your classpath.");
             throw new RuntimeException("MySQL JDBC Driver not found", e );
-        }catch(IOException e){
-            System.err.println("FATAL: Could not read database credentials configuration file.");
-            throw new RuntimeException("Could not get database credentials");
         }
     }
 
